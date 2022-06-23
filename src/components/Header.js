9312c92c4522f9f38f3591a21ed21b2e763b0797
Himanshu1771 <83176558+Himanshu1771@users.noreply.github.com>
2022-06-23 15:08:08 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <nav className="navbar navbar-dark bg-dark">
     <div className="container-fluid">
-      <a className="navbar-brand">CRUD</a>
+      <Link className="navbar-brand" to="/">CRUD</Link>
     </div>
   </nav>
   )
